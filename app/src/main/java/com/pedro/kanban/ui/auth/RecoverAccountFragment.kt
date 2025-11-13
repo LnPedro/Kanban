d095c761ff0bfd7d3c0b370d0f677dde92404bd2
LnPedro <pedroabraao1611@gmail.com>
2025-11-13 14:22:42 -0300

    Adicionando coisas novas

--- app/src/main/java/com/pedro/kanban/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/pedro/kanban/ui/auth/RecoverAccountFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.pedro.kanban.R
 import com.pedro.kanban.databinding.FragmentLoginBinding
+import com.pedro.kanban.databinding.FragmentRecoverAccountBinding
 
-class LoginFragment : Fragment() {
 
-    private var _binding: FragmentLoginBinding? = null
+class RecoverAccountFragment : Fragment() {
+
+    private var _binding: FragmentRecoverAccountBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -18,9 +20,8 @@ class LoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onDestroyView() {
