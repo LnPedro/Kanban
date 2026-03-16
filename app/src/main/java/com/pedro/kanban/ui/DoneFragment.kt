bd20f3db819ef26a0269786aefe5142963058e2c
LnPedro <pedroabraao1611@gmail.com>
2026-03-16 18:18:45 -0300

    mudanças novo tempo de comando do kaban essa semana

---
+++ app/src/main/java/com/pedro/kanban/ui/DoneFragment.kt
@@ -0,0 +1,35 @@
+package com.pedro.kanban.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.pedro.kanban.R
+import com.pedro.kanban.databinding.FragmentDoingBinding
+import com.pedro.kanban.databinding.FragmentDoneBinding
+import com.pedro.kanban.databinding.FragmentRecoverAccountBinding
+import com.pedro.kanban.databinding.FragmentSplashBinding
+
+class DoneFragment : Fragment() {
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
