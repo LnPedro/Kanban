11076193db23a5a97c90283d0c93092af799225d
LnPedro <pedroabraao1611@gmail.com>
2026-03-25 22:24:50 -0300

    Finalização

---
+++ app/src/main/java/com/pedro/kanban/ui/FormTaskFragment.kt
@@ -0,0 +1,26 @@
+package com.pedro.kanban.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pedro.kanban.R
+
+class FormTaskFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form_task, container, false)
+    }
+
+}
